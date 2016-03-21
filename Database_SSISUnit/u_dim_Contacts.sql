3bf5eea65019f5f91b42c65052e3df54f631a1d3
rarpal <rarpal@gmail.com>
2016-03-21 23:20:17 +0000

    HOME: added lookup

--- Database_SSISUnit/u_dim_Contacts.sql
+++ Database_SSISUnit/u_dim_Contacts.sql
@@ -2,10 +2,10 @@
 (
 	[key_Contact] INT NOT NULL IDENTITY , 
 	[id_Number] INT NOT NULL,
-    [Name] VARCHAR(60) NOT NULL, 
+    [Name] VARCHAR(60) NOT NULL,
+	[Telephone] VARCHAR(60) NULL,
     [Address] VARCHAR(200) NOT NULL, 
     [AddressDate] DATE NOT NULL, 
-    [is_Current] BIT NULL, 
     CONSTRAINT [pk_dim_Contacts] PRIMARY KEY NONCLUSTERED ([key_Contact])
 )
 
