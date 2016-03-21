43b33c8b240381c0b29c158cb3a397a148bd53f0
rarpal <rarpal@gmail.com>
2016-03-21 18:14:45 +0000

    OFFICE: addred split conditions

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public string Attr1 { get; set; }
+
+        public Class1() { Attr1 = "Default value";}
+    }
+}
